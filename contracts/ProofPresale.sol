265279d4369dcf7954c6782a36a97655c06b01ae
David Van Isacker <davidvanisacker@gmail.com>
2017-08-09 03:44:57 +0900

    removed constructor arguments

--- contracts/ProofPresale.sol
+++ contracts/ProofPresale.sol
@@ -1,23 +1,5 @@
 pragma solidity ^0.4.11;
 
-/**
- * @title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- */
-contract ERC20 {
-
-  uint256 public totalSupply;
-
-  function balanceOf(address who) constant returns (uint256);
-  function transfer(address to, uint256 value) returns (bool);
-  function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
-  function allowance(address owner, address spender) constant returns (uint256);
-  
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
-
-}
 
 /**
  * @title SafeMath
@@ -48,8 +30,6 @@ library SafeMath {
     return c;
   }
 }
-
-
 
 /**
  * @title Ownable
@@ -88,108 +68,6 @@ contract Ownable {
     }
   }
 
-}
-
-
-/**
- * Standard ERC20 token
- *
- * https://github.com/ethereum/EIPs/issues/20
- * Based on code by FirstBlood:
- * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
-
-/// @title Proof Presale Token (PROOFP)
-
-contract ProofPresaleToken is ERC20, Ownable {
-
-  using SafeMath for uint256;
-
-  mapping(address => uint) balances;
-  mapping (address => mapping (address => uint)) allowed;
-
-  string public name = "Proof Presale Token";
-  string public symbol = "PROOFP";
-  uint8 public decimals = 18;
-  bool public mintingFinished = false;
-
-  event Mint(address indexed to, uint256 amount);
-  event MintFinished();
-
-  function ProofPresaleToken() {}
-
-
-  // TODO : need to replace throw by 0.4.11 solidity compiler syntax
-  function() payable {
-    revert();
-  }
-
-  function transfer(address _to, uint _value) returns (bool success) {
-
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
-
-    Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  function transferFrom(address _from, address _to, uint _value) returns (bool success) {
-    var _allowance = allowed[_from][msg.sender];
-
-    balances[_to] = balances[_to].add(_value);
-    balances[_from] = balances[_from].sub(_value);
-    allowed[_from][msg.sender] = _allowance.sub(_value);
-
-    Transfer(_from, _to, _value);
-    return true;
-  }
-
-  function balanceOf(address _owner) constant returns (uint balance) {
-    return balances[_owner];
-  }
-
-  function approve(address _spender, uint _value) returns (bool success) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-  function allowance(address _owner, address _spender) constant returns (uint remaining) {
-    return allowed[_owner][_spender];
-  }
-    
-    
-  modifier canMint() {
-    require(!mintingFinished);
-    _;
-  }
-
-  /**
-   * Function to mint tokens
-   * @param _to The address that will recieve the minted tokens.
-   * @param _amount The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-
-  function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
-    totalSupply = totalSupply.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    Mint(_to, _amount);
-    return true;
-  }
-
-  /**
-   * @dev Function to stop minting new tokens.
-   * @return True if the operation was successful.
-   */
-  function finishMinting() onlyOwner returns (bool) {
-    mintingFinished = true;
-    MintFinished();
-    return true;
-  }
-
-  
-  
 }
 
 
@@ -239,44 +117,143 @@ contract Pausable is Ownable {
   }
 }
 
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 {
+
+  uint256 public totalSupply;
+  
+  function balanceOf(address _owner) constant returns (uint256);
+  function transfer(address _to, uint256 _value) returns (bool);
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool);
+  function approve(address _spender, uint256 _value) returns (bool);
+  function allowance(address _owner, address _spender) constant returns (uint256);
+  
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+}
 
 /**
- * @title Crowdsale 
- * @dev Crowdsale is a base contract for managing a token crowdsale.
- * Crowdsales have a start and end block, where investors can make
- * token purchases and the crowdsale will assign them tokens based
+ * @title ProofPresaleToken (PROOFP) 
+ * Standard Mintable ERC20 Token
+ * https://github.com/ethereum/EIPs/issues/20
+ * Based on code by FirstBlood:
+ * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+
+contract ProofPresaleToken is ERC20, Ownable {
+
+  using SafeMath for uint256;
+
+  mapping(address => uint) balances;
+  mapping (address => mapping (address => uint)) allowed;
+
+  string public constant name = "Proof Presale Token";
+  string public constant symbol = "PPT";
+  uint8 public constant decimals = 18;
+  bool public mintingFinished = false;
+
+  event Mint(address indexed to, uint256 amount);
+  event MintFinished();
+
+  function ProofPresaleToken() {}
+
+
+  function() payable {
+    revert();
+  }
+
+  function balanceOf(address _owner) constant returns (uint256) {
+    return balances[_owner];
+  }
+    
+  function transfer(address _to, uint _value) returns (bool) {
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint _value) returns (bool) {
+    var _allowance = allowed[_from][msg.sender];
+
+    balances[_to] = balances[_to].add(_value);
+    balances[_from] = balances[_from].sub(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint _value) returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) constant returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+    
+    
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
+
+  /**
+   * Function to mint tokens
+   * @param _to The address that will recieve the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
+    totalSupply = totalSupply.add(_amount);
+    balances[_to] = balances[_to].add(_amount);
+    Mint(_to, _amount);
+    return true;
+  }
+
+  /**
+   * Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner returns (bool) {
+    mintingFinished = true;
+    MintFinished();
+    return true;
+  }
+
+  
+}
+
+/**
+ * @title ProofPresale 
+ * ProofPresale allows investors to make
+ * token purchases and assigns them tokens based
  * on a token per ETH rate. Funds collected are forwarded to a wallet 
  * as they arrive.
  */
  
-contract Crowdsale is Pausable {
+contract ProofPresale is Pausable {
   using SafeMath for uint256;
 
-  // The token being sold
-  ProofPresaleToken public token;
-
-  // address where funds are collected
-  address public wallet;
-
-  // amount of raised money in wei
-  uint256 public weiRaised;
-
-  // cap above which the crowdsale is ended
-  uint256 public cap;
-
-  uint256 public minInvestment;
-
-  uint256 public rate;
-
+  ProofPresaleToken public token; 
+  
+  
+  address public wallet; //wallet towards which the funds are forwarded
+  uint256 public weiRaised; //total amount of ether raised
+  uint256 public cap; // cap above which the presale ends
+  uint256 public minInvestment; // minimum investment (10 ether)
+  uint256 public rate; // number of tokens for one ether (20)
   bool public isFinalized;
-
   string public contactInformation;
 
-  uint256 public tokenDecimals;
-
-  bool private rentrancy_lock = false;
-
-  
 
   /**
    * event for token purchase logging
@@ -293,41 +270,49 @@ contract Crowdsale is Pausable {
   event Finalized();
 
 
+  /**
+   * crowdsale constructor
+   * @param _wallet who receives invested ether
+   * @param _minInvestment is the minimum amount of ether that can be sent to the contract
+   * @param _cap above which the crowdsale is closed
+   * @param _rate is the amounts of tokens given for 1 ether
+   * @param _tokenDecimals is the number of decimals - base units - for the presale token
+   */ 
 
-
-  function Crowdsale() {
+  function ProofPresale() {
 
     token = createTokenContract();
-    wallet = 0xaCF472DBcfA46cF9E9842e2734bE2b138fB13C41;
+    wallet = '0x99892Ac6DA1b3851167Cb959fE945926bca89f09';
     rate = 20;
-    tokenDecimals = 18;
     minInvestment = (10**18)/400;  //minimum investment in wei  (=10 ether)
-    cap = 295257 * (10**18);  //cap in tokens base units (=295257 tokens)
+    cap = _cap * (10**18);  //cap in tokens base units (=295257 tokens)
+    
   }
 
-  // creates the token to be sold. 
+  // creates presale token
   function createTokenContract() internal returns (ProofPresaleToken) {
     return new ProofPresaleToken();
   }
-
 
   // fallback function to buy tokens
   function () payable {
     buyTokens(msg.sender);
   }
-
-  // low level token purchase function
+  
+  /**
+   * Low level token purchse function
+   * @param beneficiary will recieve the tokens.
+   */
   function buyTokens(address beneficiary) payable whenNotPaused {
     require(beneficiary != 0x0);
     require(validPurchase());
 
 
     uint256 weiAmount = msg.value;
-    // compute amount of tokens created
-    uint256 tokens = weiAmount.mul(rate);
-
     // update weiRaised
     weiRaised = weiRaised.add(weiAmount);
+    // compute amount of tokens created
+    uint256 tokens = weiAmount.mul(rate);
 
     token.mint(beneficiary, tokens);
     TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
@@ -335,7 +320,6 @@ contract Crowdsale is Pausable {
   }
 
   // send ether to the fund collection wallet
-  // override to create custom fund forwarding mechanisms
   function forwardFunds() internal {
     wallet.transfer(msg.value);
   }
@@ -350,6 +334,7 @@ contract Crowdsale is Pausable {
     return (notSmallAmount && withinCap);
   }
 
+  //allow owner to finalize the presale once the presale is ended
   function finalize() onlyOwner {
     require(!isFinalized);
     require(hasEnded());
@@ -360,38 +345,16 @@ contract Crowdsale is Pausable {
     isFinalized = true;
   }
 
+
   function setContactInformation(string info) onlyOwner {
       contactInformation = info;
   }
 
 
-  // @return true if crowdsale event has ended
+  //return true if crowdsale event has ended
   function hasEnded() public constant returns (bool) {
     bool capReached = (weiRaised.mul(rate) >= cap);
     return capReached;
   }
-    
-  /**
-   * 
-   * @author Remco Bloemen <remco@2π.com>
-   * @dev Prevents a contract from calling itself, directly or indirectly.
-   * @notice If you mark a function `nonReentrant`, you should also
-   * mark it `external`. Calling one nonReentrant function from
-   * another is not supported. Instead, you can implement a
-   * `private` function doing the actual work, and a `external`
-   * wrapper marked as `nonReentrant`.
-   */
-  
-  // TODO need to set nonReentrant modifier where necessary
-
-  modifier nonReentrant() {
-    require(!rentrancy_lock);
-    rentrancy_lock = true;
-    _;
-    rentrancy_lock = false;
-  }
-
 
 }
-
-
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ var Pausable = artifacts.require('./Pausable.sol');
 var Contactable = artifacts.require('./Contactable.sol');
 var ERC20 = artifacts.require('./ERC20.sol');
 var ProofPresaleToken = artifacts.require('./ProofPresaleToken.sol');
-var Crowdsale = artifacts.require('./Crowdsale.sol');
+var ProofPresale = artifacts.require('./ProofPresale.sol');
 
 
 var wallet = '0xacf472dbcfa46cf9e9842e2734be2b138fb13c41'
@@ -12,28 +12,28 @@ var minInvestment = (10 ** 18) / 280;
 var tokenCap = 295257;
 var rate = 20;
 var decimals = 18;
+var gas = 300000
+var gasPrice = 13*(10**9);
 
-module.exports = function(deployer) {
-  deployer.deploy(SafeMath, {gas: 3000000, gasPrice: 13 * 10 ** 9 });
-  deployer.link(SafeMath, Crowdsale);
-  deployer.link(SafeMath, ProofPresaleToken);
-  deployer.deploy(Ownable, {gas: 3000000, gasPrice: 13 * 10 ** 9  });
-  deployer.deploy(Pausable, {gas: 3000000, gasPrice: 13 * 10 ** 9  });
-  deployer.deploy(Contactable, {gas: 3000000, gasPrice: 13 * 10 ** 9  });
-  deployer.deploy(ProofPresaleToken, {gas: 3000000, gasPrice: 13 * 10 ** 9  });
-  deployer.deploy(Crowdsale, wallet, minInvestment, tokenCap, rate, decimals, {gas: 3000000, gasPrice: 13*10**9 });
-};
-
-
- /**
+/**
 * Standard constructor arguments for Crowdsale contract:
 * @param wallet that ultimately receives ether sent to the crowdsale contract
 * @param minimum investment in ether
 * @param crowdsale token cap
 * @param rate : amounts of tokens received for 1 ether invested
-* @param decimals
-* deployer.deploy(Crowdsale, '0x00000000000000', 10, 295257, 20, 18, {gas: 3000000, gasPrice: 1000000000});
+* deployer.deploy(ProofPresale, '0x00000000000000', 10, 295257, 20, 18, {gas: 3000000, gasPrice: 1000000000});
 */ 
+
+module.exports = function(deployer) {
+  deployer.deploy(SafeMath, {gas: gas, gasPrice: gasPrice});
+  deployer.link(SafeMath, Crowdsale);
+  deployer.link(SafeMath, ProofPresaleToken);
+  deployer.deploy(Ownable, {gas: gas, gasPrice: gasPrice });
+  deployer.deploy(Pausable, {gas: gas, gasPrice: gasPrice });
+  deployer.deploy(Contactable, {gas: gas, gasPrice: gasPrice });
+  deployer.deploy(ProofPresaleToken, {gas: gas, gasPrice: gasPrice });
+  deployer.deploy(ProofPresale, wallet, minInvestment, tokenCap, rate, {gas: gas, gasPrice: gasPrice });
+};
 
 
 
